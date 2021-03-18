24b70671091dbfe626e8be13b83bbfebd8f31cef
Svenja Meyer <svenja.meyer@desy.de>
2021-03-18 14:52:00 +0100

    Rework writefids.py for better overview and exception handling

--- packer/src/writebfids.py
+++ packer/src/writebfids.py
@@ -62,140 +62,162 @@ def _sha1(filepath):
 
 
 def main(configfile='/etc/dcache/container.conf'):
+    # global variables
     global running
+    # are they needed?
+    global archiveUser
+    global archiveMode
+    global mountPoint
+    global dataRoot
+    global mongoUri
+    global mongoDb
 
-    logger = logging.getLogger()
-    log_handler = None
+    # general variables
     checksum_calculation = {"md5": _md5,
                             "adler32": _adler32,
                             "sha1": _sha1
                             }
+    logger = logging.getLogger()
+    log_handler = None
+    # Make this configurable (via MongoDB)
+    osm_type = "osm"
+    osm_name = "osm"
 
-    try:
-        while running:
-            configuration = parser.RawConfigParser(defaults={'scriptId': 'pack', 'mongoUri': 'mongodb://localhost/',
-                                                             'mongoDb': 'smallfiles', 'logLevel': 'ERROR'})
+    while running:
+        # Read configuration
+        configuration = parser.RawConfigParser(defaults={'scriptId': 'pack', 'mongoUri': 'mongodb://localhost/',
+                                                         'mongoDb': 'smallfiles', 'logLevel': 'ERROR'})
+
+        # Configure parameters from configuration file
+        try:
             configuration.read(configfile)
-
-            global archiveUser
-            global archiveMode
-            global mountPoint
-            global dataRoot
-            global mongoUri
-            global mongoDb
-            type = "osm"  # Make it configurable
-            name = "osm"
-
             script_id = configuration.get('DEFAULT', 'script_id')
-
             log_level_str = configuration.get('DEFAULT', 'log_level')
-            log_level = getattr(logging, log_level_str.upper(), None)
-            logger.setLevel(log_level)
-
-            if log_handler is not None:
-                log_handler.close()
-                logger.removeHandler(log_handler)
-
-            log_handler = logging.handlers.WatchedFileHandler(f'/var/log/dcache/writebfids-{script_id}.log')
-            formatter = logging.Formatter('%(asctime)s %(name)-10s %(levelname)-8s %(message)s')
-            log_handler.setFormatter(formatter)
-            logger.addHandler(log_handler)
-
             archiveUser = configuration.get('DEFAULT', 'archiveUser')
             archiveMode = configuration.get('DEFAULT', 'archiveMode')
             mountPoint = configuration.get('DEFAULT', 'mount_point')
             dataRoot = configuration.get('DEFAULT', 'data_root')
             mongoUri = configuration.get('DEFAULT', 'mongo_url')
             mongoDb = configuration.get('DEFAULT', 'mongo_db')
+        except FileNotFoundError as e:
+            logger.critical(f'Configuration file "{configfile}" not found. Exiting now.')
+            sys.exit(1)
+        except parser.NoSectionError as e:
+            # Section DEFAULT nicht gefunden
+            logger.critical(f'Section [DEFAULT] was not found in "{configfile}". This section is mandatory, exiting now.')
+            sys.exit(1)
+        except parser.NoOptionError as e:
+            # Option (e.g. data_root) not found in section
+            logger.critical(f'An option is missing in section [DEFAULT] of file "{configfile}", exiting now: {e}')
+            sys.exit(1)
+        except parser.MissingSectionHeaderError as e:
+            # file doesn't contain section header
+            logger.critical(f'The file "{configfile}" doesn\'t contain section headers. Exiting now')
+            sys.exit(1)
+        except parser.ParsingError as e:
+            # error while parsing
+            logger.critical(f'There was an error parsing while parsing the configuration "{configfile}", exiting now: {e}')
+            sys.exit(1)
+        except parser.Error as e:
+            # Base class of other configparser exceptions, maybe delete this
+            logger.critical(f'An error occurred while reading the configuration file {configfile}, exiting now: {e}')
+            sys.exit(1)
 
-            logging.info(f'Successfully read configuration from file {configfile}.')
+        log_level = getattr(logging, log_level_str.upper(), None)
+        logger.setLevel(log_level)
 
-            try:
-                client = MongoClient(mongoUri)
-                db = client[mongoDb]
-                logging.info("Established db connection")
+        if log_handler is not None:
+            log_handler.close()
+            logger.removeHandler(log_handler)
 
-                with db.archives.find() as db_archives:
-                    for archive in db_archives:
-                        if not running:
-                            logger.info("Exiting")
-                            sys.exit(1)
-                        try:
-                            zip_file = ZipFile(archive['path'], mode="r", allowZip64=True)
-                            for f in zip_file.filelist:
-                                filerecord = db.files.find_one({'pnfsid': f.filename,
-                                                                'state': f"archived: {archive['path']}"})
-                                if not filerecord:
-                                    logger.error(f"File {f.filename} in ZipFile is not in MongoDB! "
-                                                 f"Creating failure entry")
-                                    db.failures.insert_one({'archivePath': archive['path'], 'pnfsid': f.filename})
-                            filelist = zip_file.filelist
-                            zip_file.close()
+        log_handler = logging.handlers.WatchedFileHandler(f'/var/log/dcache/writebfids-{script_id}.log')
+        formatter = logging.Formatter('%(asctime)s %(name)-10s %(levelname)-8s %(message)s')
+        log_handler.setFormatter(formatter)
+        logger.addHandler(log_handler)
 
-                            auth = HTTPBasicAuth('admin', 'dickerelch')
-                            url = f"https://localhost:2881/archives/{os.path.basename(archive['path'])}"
+        logging.info(f'Successfully read configuration from file {configfile}.')
 
-                            headers = {"Content-type": "application/octet-stream"}
-                            response = requests.put(url, data=open(archive['path'], 'rb'),
-                                                    auth=auth, verify=False, headers=headers)
-                            if response.status_code == 201 or response.status_code == 200:
-                                logger.info(f"Archive uploaded successfully")
-                            else:
-                                logger.error(f"Uploading file failed: {response.status_code} -- {response}")
-                                continue
+        # Connect to database and get archives
+        client = MongoClient(mongoUri)
+        db = client[mongoDb]
+        with db.archives.find() as db_archives:
+            for archive in db_archives:
+                if not running:
+                    logger.info("Exiting")
+                    sys.exit(0)
+                # Open ZIP-File and get filelist
+                try:
+                    zip_file = ZipFile(archive['path'], mode="r", allowZip64=True)
+                    archive_pnfsidlist = [f.filename for f in zip_file.filelist]
+                except BadZipfile:
+                    logger.warning(f"Archive {archive['path']} is not ready yet. Will try again later.")
+                    continue
+                except FileNotFoundError:
+                    logger.error(f"Container {archive['path']} could not be found on local disk. Files, that should be "
+                                 f"in this archive, are now reset to be packed again.")
+                    for pnfsid in archive_pnfsidlist:
+                        file_result = db.files.find_one({"pnfsid": pnfsid})
+                        file_result['state'] = "new"
+                        db.files.replace_one({"pnfsid", pnfsid}, file_result)
+                        continue
+                finally:
+                    zip_file.close()
 
-                            headers = {"Want-Digest": "ADLER32,MD5,SHA1"}
-                            response = requests.head(f"https://localhost:2881/archives/{os.path.basename(archive['path'])}",
-                                                     verify=False, auth=auth, headers=headers)
-                            pnfsid = response.headers.get("ETag").split('_')[0].replace('"', '')
-                            logger.info(f"PNFSID of Container {archive['path']} is {pnfsid}")
+                # Check if every file that should be in the archive is there
+                db_pnfsidlist = db.files.find({"state": f"archived: {archive['path']}"})
+                db_pnfsidlist = [f['pnfsid'] for f in db_pnfsidlist]
+                sym_diff_filelist = set(archive_pnfsidlist).symmetric_difference(set(db_pnfsidlist))
+                for pnfsid in sym_diff_filelist:
+                    if pnfsid in archive_pnfsidlist:
+                        logger.warning(f"File {pnfsid} is in archive {archive['path']}, but not in MongoDB! Logging failure")
+                        db.failures.insert_one({'archivePath': archive['path'], 'pnfsid': pnfsid})
+                    elif pnfsid in db_pnfsidlist:
+                        logger.warning(f"File {pnfsid} is listed in MongoDB for archive {archive['path']}, but isn't "
+                                       f"there. Resetting file for packing into new archive")
+                        db_file = db.files.find_one({"pnfsid": pnfsid})
+                        db_file['state'] = "new"
+                        db.files.replace_one({"pnfsid": pnfsid}, db_file)
 
-                            checksum_type, remote_checksum = response.headers.get("Digest").split('=', 1)
-                            if checksum_type not in checksum_calculation.keys():
-                                logger.error(f"Checksum mechanism {checksum_type} for file {archive['path']} is not supported")
-                                raise NotImplementedError()
-                            local_checksum = checksum_calculation[checksum_type](archive['path'])
+                # Upload zip-file to dCache
+                auth = HTTPBasicAuth('admin', 'dickerelch')  # TODO make it configurable
+                url = f"https://localhost:2881/archives/{os.path.basename(archive['path'])}"  # TODO make it configurable
+                headers = {"Content-type": "application/octet-stream"}
+                response = requests.put(url, data=open(archive['path'], 'rb'), verify=False, auth=auth, headers=headers)
+                if response.status_code not in (200, 201):
+                    print()  # TODO add functionality
+                    continue
 
-                            if remote_checksum == local_checksum:
-                                for file in filelist:
-                                    file_entry = db.files.find_one({'pnfsid': file.filename})
-                                    if file_entry is None:
-                                        logger.error(f"No record found for {file.filename}")
-                                    logger.debug(f"Update file {file.filename} now.")
-                                    logger.debug(f"type: {type} name: {name} bfid: {file.filename}:{pnfsid}")
-                                    archive_url = f"{type}://{name}/?store={file_entry['store']}&group=" \
-                                                  f"{file_entry['group']}&bfid={file.filename}:{pnfsid}"
-                                    logger.debug(f"ArchiveUrl: {archive_url}")
-                                    file_entry['archiveUrl'] = archive_url
-                                    file_entry['state'] = f"verified: {archive['path']}"
-                                    db.files.replace_one({"pnfsid": file.filename}, file_entry)
-                            else:
-                                logger.error("Checksums of local file and uploaded file doesn't match!")
-                            os.remove(archive['path'])
-                            db.archives.delete_one({"path": archive['path']})
-                        except BadZipfile as e:
-                            logger.warning(f"Archive {archive['path']} is not ready yet. Will try again later.")
+                # Request PNFSID and Checksum from dCache, calculate local checksum
+                headers = {"Want-Digest": "ADLER32,MD5,SHA1"}
+                response = requests.head(url, verify=False, auth=auth, headers=headers)
+                if response not in (201, 200):
+                    print()  # TODO add functionality
+                pnfsid = response.headers.get("ETag").split('_')[0].replace('"', '')
+                checksum_type, remote_checksum = response.headers.get("Digest").split('=', 1)
+                if checksum_type not in checksum_calculation.keys():
+                    raise NotImplementedError()
+                local_checksum = checksum_calculation[checksum_type](archive['path'])
 
-            except errors.ConnectionFailure as e:
-                logger.warning(f"Connection to DB failed: {e}")
-            except errors.OperationFailure as e:
-                logger.warning(f"Could not create cursor: {e}")
-            except Exception as e:
-                logger.error(f"Unexpected error: {e}")
-            finally:
-                if client is not None:
-                    client.close()
+                # Compare Checksums
+                if remote_checksum == local_checksum:
+                    for file_pnfsid in archive_pnfsidlist:
+                        file_entry = db.files.find_one({'pnfsid': file_pnfsid})
+                        if file_entry is None:
+                            continue
+                        archive_url = f"{osm_type}://{osm_name}/?store={file_entry['store']}&group=" \
+                                      f"{file_entry['group']}&bfid={file_pnfsid}:{pnfsid}"
+                        file_entry['archiveUrl'] = archive_url
+                        file_entry['state'] = f"verified: {archive['path']}"
+                        db.files.replace_one({"pnfsid": file_pnfsid}, file_entry)
+                else:
+                    print()  # TODO add functionality
 
-            logging.info("Processed all archive entries. Sleeping 60 seconds.")
+                # Cleanup
+                os.remove(archive['path'])
+                db.archives.delete_one({"path": archive['path']})
+            if client is not None:
+                client.close()
             time.sleep(60)
-
-    except parser.NoOptionError as e:
-        print(f"Missing option: {e}")
-        logger.error(f"Missing option: {e}")
-    except parser.Error as e:
-        print(f"Error reading configfile {configfile}: {e}")
-        logger.error(f"Error reading configfile {configfile}: {e}")
-        sys.exit(2)
 
 
 if __name__ == '__main__':
