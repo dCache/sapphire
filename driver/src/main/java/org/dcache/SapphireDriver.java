019eab392d32170495b1273fb4ff250e650268c8
Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
2021-03-26 21:09:45 +0100

    sapphire: SapphireDriver#cancel should not fail the request.
    as pool cancels the requests, it takes care to failed them if required.
    
    (cherry picked from commit e6d5bbf25645fc06ea4878c338f6f6ef34dfa19d)
    Signed-off-by: Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>

--- driver/src/main/java/org/dcache/SapphireDriver.java
+++ driver/src/main/java/org/dcache/SapphireDriver.java
@@ -110,7 +110,6 @@ public class SapphireDriver implements NearlineStorage
                 .ifPresent(request ->  {
                     if (flushRequestQueue.removeIf(byUUID)) {
                         files.deleteOne(new Document("pnfsid", request.getFileAttributes().getPnfsId().toString()));
-                        request.failed(new CancellationException());
                     }
                 });
     }
