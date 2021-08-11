9bf9a009130e86f5387e56cbc54ea989d2bcd895
Svenja Meyer <svenja.meyer@desy.de>
2021-08-11 09:15:13 +0200

    FileServer: Add StageServlet

--- driver/src/main/java/org/dcache/FileServer.java
+++ driver/src/main/java/org/dcache/FileServer.java
@@ -3,17 +3,32 @@ package org.dcache;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.InetAccessHandler;
 import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import javax.servlet.MultipartConfigElement;
+import java.nio.file.Paths;
 
 public class FileServer {
     Server server;
     private static final Logger _log = LoggerFactory.getLogger(SapphireDriver.class);
 
-    public FileServer (int port, String[] whitelist, String host) {
+    public FileServer (int port, String[] whitelist) {
         server = new Server(port);
         ServletContextHandler handler = new ServletContextHandler(server, "/sapphire");
         handler.addServlet(FileServlet.class, "/v1");
+
+        ServletHolder stageServletHolder = new ServletHolder(new StageServlet());
+        String location = Paths.get("").toString();
+
+        long maxFilesize = -1L;
+        long maxRequestsize = -1L;
+        int filesizeThreshold = 64*1024;
+
+        MultipartConfigElement multipartConfigElement = new MultipartConfigElement(location, maxFilesize, maxRequestsize, filesizeThreshold);
+        stageServletHolder.getRegistration().setMultipartConfig(multipartConfigElement);
+        handler.addServlet(stageServletHolder, "/stage");
 
         InetAccessHandler accessHandler = new InetAccessHandler();
         for(String ip : whitelist) {
