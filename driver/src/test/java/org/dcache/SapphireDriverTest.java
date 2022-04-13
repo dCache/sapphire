907c6dcd6933b7693cd4fca4fb627c8009232e04
Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
2022-04-13 18:29:04 +0200

    test: generate self-signed host certificates

--- driver/src/test/java/org/dcache/SapphireDriverTest.java
+++ driver/src/test/java/org/dcache/SapphireDriverTest.java
@@ -10,6 +10,21 @@ import de.bwaldvogel.mongo.backend.memory.MemoryBackend;
 
 import diskCacheV111.vehicles.GenericStorageInfo;
 import diskCacheV111.vehicles.StorageInfo;
+import eu.emi.security.authn.x509.impl.CertificateUtils;
+import org.bouncycastle.asn1.x500.X500Name;
+import org.bouncycastle.asn1.x509.BasicConstraints;
+import org.bouncycastle.asn1.x509.ExtendedKeyUsage;
+import org.bouncycastle.asn1.x509.Extension;
+import org.bouncycastle.asn1.x509.KeyPurposeId;
+import org.bouncycastle.asn1.x509.KeyUsage;
+import org.bouncycastle.asn1.x509.SubjectPublicKeyInfo;
+import org.bouncycastle.cert.X509CertificateHolder;
+import org.bouncycastle.cert.X509v3CertificateBuilder;
+import org.bouncycastle.cert.jcajce.JcaX509CertificateConverter;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.operator.ContentSigner;
+import org.bouncycastle.operator.OperatorCreationException;
+import org.bouncycastle.operator.jcajce.JcaContentSignerBuilder;
 import org.bson.BasicBSONObject;
 import org.bson.Document;
 import org.dcache.pool.nearline.spi.FlushRequest;
@@ -18,9 +33,19 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.math.BigInteger;
 import java.net.InetSocketAddress;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.security.GeneralSecurityException;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.SecureRandom;
+import java.util.Date;
 import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
@@ -28,6 +53,8 @@ import java.util.concurrent.CancellationException;
 import java.util.concurrent.TimeUnit;
 
 import static com.mongodb.client.model.Filters.eq;
+import static java.nio.file.StandardOpenOption.*;
+import static java.nio.file.StandardOpenOption.WRITE;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
@@ -43,12 +70,20 @@ public class SapphireDriverTest {
     private SapphireDriver sapphireDriver;
     private FlushRequest flushRequest;
 
+    private File keyFile;
+    private File certFile;
+
     @BeforeEach
-    public void setUp() {
+    public void setUp() throws IOException, GeneralSecurityException, OperatorCreationException {
         mongoServer = new MongoServer(new MemoryBackend());
         InetSocketAddress serverAddress = mongoServer.bind();
         String connectionString = "mongodb://" + new ServerAddress(serverAddress).toString();
         mongoClient = MongoClients.create(connectionString);
+
+        keyFile = File.createTempFile("hostkey-", ".pem");
+        certFile = File.createTempFile("hostcert-", ".pem");
+
+        generateSelfSignedCert();
 
         sapphireDriver = new SapphireDriver("foo", "bar");
 
@@ -57,7 +92,9 @@ public class SapphireDriverTest {
                 "database", "hsm",
                 "period", "1",
                 "period_unit", TimeUnit.SECONDS.name(),
-                "port", "12300-12500"
+                "port", "12300-12500",
+                "cert", certFile.getAbsolutePath(),
+                "key", keyFile.getAbsolutePath()
         );
 
         fileCollection = mongoClient.getDatabase("hsm").getCollection("files");
@@ -152,5 +189,53 @@ public class SapphireDriverTest {
         try {
             TimeUnit.SECONDS.sleep(sec);
         } catch (InterruptedException e) {}
+    }
+
+
+    private void generateSelfSignedCert()
+            throws GeneralSecurityException, OperatorCreationException, IOException {
+
+        KeyPairGenerator keyPairGenerator = KeyPairGenerator.getInstance("RSA", new BouncyCastleProvider());
+        keyPairGenerator.initialize(2048, new SecureRandom());
+        KeyPair keyPair = keyPairGenerator.generateKeyPair();
+
+        long notBefore = System.currentTimeMillis();
+        long notAfter = notBefore + TimeUnit.DAYS.toMillis(1);
+
+        X500Name subjectDN = new X500Name("CN=localhost, O=dCache.org");
+        X500Name issuerDN = subjectDN;
+
+        SubjectPublicKeyInfo subjectPublicKeyInfo =
+                SubjectPublicKeyInfo.getInstance(keyPair.getPublic().getEncoded());
+
+        X509v3CertificateBuilder certificateBuilder = new X509v3CertificateBuilder(issuerDN,
+                BigInteger.ONE,
+                new Date(notBefore),
+                new Date(notAfter), subjectDN,
+                subjectPublicKeyInfo)
+                .addExtension(Extension.basicConstraints, true, new BasicConstraints(true))
+                .addExtension(Extension.keyUsage, true, new KeyUsage(KeyUsage.digitalSignature | KeyUsage.keyEncipherment))
+                .addExtension(Extension.extendedKeyUsage, true, new ExtendedKeyUsage(
+                                new KeyPurposeId[] {KeyPurposeId.id_kp_clientAuth, KeyPurposeId.id_kp_serverAuth}
+                        )
+                );
+
+        String signatureAlgorithm = "SHA256WithRSA";
+
+        // sign with own key
+        ContentSigner contentSigner = new JcaContentSignerBuilder(signatureAlgorithm)
+                .build(keyPair.getPrivate());
+
+        X509CertificateHolder certificateHolder = certificateBuilder.build(contentSigner);
+        var cert = new JcaX509CertificateConverter().getCertificate(certificateHolder);
+
+        try (OutputStream certOut = Files.newOutputStream(
+                certFile.toPath(), CREATE, TRUNCATE_EXISTING,
+                WRITE); OutputStream keyOut = Files.newOutputStream(keyFile.toPath(), CREATE,
+                TRUNCATE_EXISTING, WRITE)) {
+
+            CertificateUtils.saveCertificate(certOut, cert, CertificateUtils.Encoding.PEM);
+            CertificateUtils.savePrivateKey(keyOut, keyPair.getPrivate(), CertificateUtils.Encoding.PEM, null, null);
+        }
     }
 }
