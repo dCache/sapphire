cbb706934bb47dc63fd9dd0c5b0f05702010ef6c
Svenja Meyer <svenja.meyer@desy.de>
2022-11-17 09:14:31 +0100

    stage-files.py: Remove redundant log message

--- packer/src/stage-files.py
+++ packer/src/stage-files.py
@@ -197,7 +197,6 @@ def unpack_upload_file(archive, pnfsid, filepath, url, mongo_db, macaroon):
                  times=(datetime.datetime.now().timestamp(), stat.st_mtime))  # Update access time of archive
 
         if response.status_code == 201:
-            logger.info(f"File {pnfsid} was uploaded to dCache successfully")
             try:
                 mongo_db.stage.update_one({"pnfsid": pnfsid}, {"$set": {"status": "done"}})
             except (pymongo.errors.ConnectionFailure, pymongo.errors.ServerSelectionTimeoutError) as e:
